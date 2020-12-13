7ae4ab2ac7171eb252ac405f9925f565adccd65b
0712023 <0712023@hanmail.net>
2020-12-13 23:45:26 +0900

    ver 1.4

--- CallPython3.go
+++ CallPython3.go
@@ -6,15 +6,14 @@ package main
        "strconv"
    )
    func main() {
-       cmd := exec.Command("sudo", "python3","/home/pi/workspace/empty.py")
-       outputn, err := cmd.Output()
+       outputn, err := exec.Command("sudo", "python","/home/pi/workspace/getdata.py").Output()
        if (err != nil) {
            fmt.Println(err)
        }
        output := string(outputn[:len(outputn)-1])
        outputf, errf := strconv.ParseFloat(output, 64)
        if (errf != nil) {
-           fmt.Println(err)
+          fmt.Println(err)
        }
        fmt.Println(outputf)
    }
