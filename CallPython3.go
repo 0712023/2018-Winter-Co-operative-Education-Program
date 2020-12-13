f945daf4ff526ad8a9fe4116b722279879936338
0712023 <0712023@hanmail.net>
2020-12-13 23:45:06 +0900

    ver 1.3

--- CallPython3.go
+++ CallPython3.go
@@ -3,14 +3,18 @@ package main
    import (
        "os/exec"
        "fmt"
+       "strconv"
    )
-
    func main() {
        cmd := exec.Command("sudo", "python3","/home/pi/workspace/empty.py")
        outputn, err := cmd.Output()
-       output := string(outputn[:len(outputn)-1])
        if (err != nil) {
            fmt.Println(err)
        }
-       fmt.Println(string(output))
+       output := string(outputn[:len(outputn)-1])
+       outputf, errf := strconv.ParseFloat(output, 64)
+       if (errf != nil) {
+           fmt.Println(err)
+       }
+       fmt.Println(outputf)
    }
